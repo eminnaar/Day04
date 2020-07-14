410f6b44d010c01ead51928332e34f822d5491b1
eminnaar <67954571+eminnaar@users.noreply.github.com>
2020-07-14 12:52:45 +0200

    Completed ex00

--- ex00/ft_iterative_factorial.c
+++ ex00/ft_iterative_factorial.c
@@ -9,22 +9,12 @@
 /*   Updated: 2020/07/13 14:37:12 by eminnaar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+int ft_iterative_factorial(int nb);
 
-#include <stdio.h>
-
-int ft_recursive_factorial(int nb);
-
-int ft_recursive_factorial(int nb)
+int ft_iterative_factorial(int nb)
 {
     if (nb <= 0)
         return 0;
     else
-        return 1 + ft_recursive_factorial(nb - 1);
-}
-
-int     main()
-{
-    int nb = -5;
-    printf("%d", ft_recursive_factorial(nb));
-    return 0;
+        return 1 + ft_iterative_factorial(nb - 1);
 }
